5b017b52bccf021c1608a9522c79724a72fa62d9
Guillaume Benard <guillaume.benard@epitech.eu>
2018-10-07 21:00:45 -0400

    [Groups] Add Group owner in database scheme
    - Add owner_id foreign key
    - rewrite migration into one

--- sources/model/entity/Group.ts
+++ sources/model/entity/Group.ts
@@ -2,7 +2,7 @@
 // Created by benard-g on 2018//07
 //
 
-import {Column, Entity, ManyToMany, PrimaryGeneratedColumn} from "typeorm";
+import {Column, Entity, JoinColumn, ManyToMany, ManyToOne, PrimaryGeneratedColumn} from "typeorm";
 import {User} from "./User";
 
 @Entity("groups")
@@ -14,12 +14,23 @@ export class Group {
     @Column()
     name: string;
 
+    @ManyToOne(type => User)
+    @JoinColumn({name: "owner_id"})
+    ownerId: number;
+
     @ManyToMany(type => User)
-    users: User[];
+    members: User[];
 
     @Column({name: "created_at", type: "timestamp with time zone", default: () => "CURRENT_TIMESTAMP"})
     createdAt: Date;
 
     @Column({name: "updated_at", type: "timestamp with time zone", default: () => "CURRENT_TIMESTAMP"})
     updatedAt: Date;
+
+
+    public constructor(name: string, ownerId: number, members: User[]) {
+        this.name = name;
+        this.ownerId = ownerId;
+        this.members = members;
+    }
 }
