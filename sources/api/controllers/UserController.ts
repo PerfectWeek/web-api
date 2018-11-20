34c25fb7afe32f50ccd1e592a3282f971db1981f
benard_g <guillaume.benard@epitech.eu>
2018-11-20 17:46:17 -0500

    Reorder UserController functions

--- sources/api/controllers/UserController.ts
+++ sources/api/controllers/UserController.ts
@@ -19,59 +19,84 @@ import {Group} from "../../model/entity/Group";
 
 
 //
-// Delete user
+// Create a new User and save it in the DataBase
 //
-export async function deleteUser(req: Request, res: Response) {
-    let user: User = getRequestingUser(req);
+export async function createUser(req: Request, res: Response) {
+    if (!req.body.pseudo || !req.body.password || !req.body.email)
+        throw new ApiException(400, "Invalid request");
 
-    if (user.pseudo !== req.params.pseudo) {
-        return res.status(403).json({
-            message: "Action not allowed"
-        });
+    // Check if user exists before creating PendingUser
+    const connection = await DbConnection.getConnection();
+    const UserRepository = connection.getRepository(User);
+
+    const userExists = await UserRepository.createQueryBuilder('user')
+        .where("user.email = :email", { email: req.body.email })
+        .orWhere("user.pseudo = :pseudo", { pseudo: req.body.pseudo }).getOne();
+
+    if (userExists) {
+        throw new ApiException(409, "Pseudo or email already exists");
     }
 
-    const conn = await DbConnection.getConnection();
-    const userRepository = conn.getRepository(User);
-    const groupsToUsersRepository = conn.getRepository(GroupsToUsers);
-    await User.deleteUser(userRepository, groupsToUsersRepository, user.id);
-
-    return res.status(200).json({
-        message: "User deleted"
-    });
-}
-
-//
-// Edit a user's information
-//
-export async function editUser(req: Request, res: Response) {
-    let user: User = getRequestingUser(req);
-    const old_pseudo = user.pseudo;
-
-    if (user.pseudo !== req.params.pseudo) {
-        throw new ApiException(403, "Action forbidden");
-    }
-
-    user.pseudo = req.body.pseudo || user.pseudo;
-    user.email = req.body.email || user.email;
-
-    if (!user.isValid()) {
+    const validation_link: string = AccountVerification.generateLink();
+    const user: PendingUser = new PendingUser(
+        req.body.pseudo,
+        req.body.email,
+        await PendingUser.cipherPassword(req.body.password),
+        validation_link
+    );
+    if (!user.isValid())
         throw new ApiException(400, "Invalid fields in User");
-    }
 
+    // Save the created User
     const conn = await DbConnection.getConnection();
-
-    if (old_pseudo !== user.pseudo
-        && await conn.getRepository(User).findOne({where: {pseudo: user.pseudo}})) {
-        throw new ApiException(400, "Invalid pseudo: already taken");
-    }
-
     await conn.manager.save(user);
 
-    return res.status(200).json({
-        message: "User updated",
+    let reqUrl = process.env.API_HOST || getReqUrl(req);
+    if (!reqUrl.endsWith('/')) {
+        reqUrl += '/';
+    }
+
+    const link = reqUrl + 'auth/validate-email/' + validation_link;
+    EmailSender.sendEmail(user.email, 'Account Verification', link);
+
+    let response : any = {
+        message:
+            'A link has been sent to your email address, please click on it in order to confirm you email',
+        user: UserView.formatPendingUser(user)
+    };
+
+    if (!process.env.EMAIL_ENABLED) {
+        response['link'] = link;
+    }
+    return res.status(201).json(response);
+}
+
+
+//
+// Validate email for a specific User
+//
+export async function confirmUserEmail(req: Request, res: Response) {
+    const conn = await DbConnection.getConnection();
+    const pendingUserRepository = conn.getRepository(PendingUser);
+    const pendingUser = await pendingUserRepository.findOne({where: {validationUuid: req.params.uuid}});
+
+    if (!pendingUser)
+        throw new ApiException(404, "User not found");
+
+    const user: User = new User(
+        pendingUser.pseudo,
+        pendingUser.email,
+        pendingUser.cipheredPassword,
+    );
+    await conn.manager.save(user);
+    await conn.manager.remove(pendingUser);
+
+    return res.status(201).json({
+        message: "User has been successfully created",
         user: UserView.formatUser(user)
     });
 }
+
 
 //
 // Log a user in and return a session token
@@ -97,83 +122,6 @@ export async function login(req: Request, res: Response) {
     });
 }
 
-//
-// Create a new User and save it in the DataBase
-//
-export async function createUser(req: Request, res: Response) {
-    if (!req.body.pseudo || !req.body.password || !req.body.email)
-        throw new ApiException(400, "Invalid request");
-
-    // Check if user exists before creating PendingUser
-    const connection = await DbConnection.getConnection();
-    const UserRepository = connection.getRepository(User);
-
-    const userExists = await UserRepository.createQueryBuilder('user')
-        			     .where("user.email = :email", { email: req.body.email })
-	    			     .orWhere("user.pseudo = :pseudo", { pseudo: req.body.pseudo }).getOne();
-    
-    if (userExists) {
-        throw new ApiException(409, "Pseudo or email already exists");
-    }
-
-    const validation_link: string = AccountVerification.generateLink();
-    const user: PendingUser = new PendingUser(
-        req.body.pseudo,
-        req.body.email,
-        await PendingUser.cipherPassword(req.body.password),
-        validation_link
-    );
-    if (!user.isValid())
-        throw new ApiException(400, "Invalid fields in User");
-    
-    // Save the created User
-    const conn = await DbConnection.getConnection();
-    await conn.manager.save(user);
-
-    let reqUrl = process.env.API_HOST || getReqUrl(req);
-    if (!reqUrl.endsWith('/')) {
-    	reqUrl += '/';
-    }
-
-    const link = reqUrl + 'auth/validate-email/' + validation_link;
-    EmailSender.sendEmail(user.email, 'Account Verification', link);
-
-    let response : any = {
-      message:
-          'A link has been sent to your email address, please click on it in order to confirm you email',
-      user: UserView.formatPendingUser(user)
-    };
-
-    if (!process.env.EMAIL_ENABLED) {
-       response['link'] = link;
-    }
-    return res.status(201).json(response);
-}
-
-//
-// Validate email for a specific user
-//
-export async function confirmUserEmail(req: Request, res: Response) {
-    const conn = await DbConnection.getConnection();
-    const pendingUserRepository = conn.getRepository(PendingUser);
-    const pendingUser = await pendingUserRepository.findOne({where: {validationUuid: req.params.uuid}});
-
-    if (!pendingUser)
-        throw new ApiException(404, "User not found");
-
-    const user: User = new User(
-        pendingUser.pseudo,
-        pendingUser.email,
-        pendingUser.cipheredPassword,
-    );
-    await conn.manager.save(user);
-    await conn.manager.remove(pendingUser);
-
-    return res.status(201).json({
-        message: "User has been successfully created",
-        user: UserView.formatUser(user)
-    });
-}
 
 //
 // Get information about a specific User
@@ -191,6 +139,64 @@ export async function getUser(req: Request, res: Response) {
         user: UserView.formatUser(user)
     });
 }
+
+
+//
+// Edit a User's information
+//
+export async function editUser(req: Request, res: Response) {
+    let user: User = getRequestingUser(req);
+    const old_pseudo = user.pseudo;
+
+    if (user.pseudo !== req.params.pseudo) {
+        throw new ApiException(403, "Action not allowed");
+    }
+
+    user.pseudo = req.body.pseudo || user.pseudo;
+    user.email = req.body.email || user.email;
+
+    if (!user.isValid()) {
+        throw new ApiException(400, "Invalid fields in User");
+    }
+
+    const conn = await DbConnection.getConnection();
+
+    if (old_pseudo !== user.pseudo
+        && await conn.getRepository(User).findOne({where: {pseudo: user.pseudo}})) {
+        throw new ApiException(400, "Invalid pseudo: already taken");
+    }
+
+    await conn.manager.save(user);
+
+    return res.status(200).json({
+        message: "User updated",
+        user: UserView.formatUser(user)
+    });
+}
+
+
+//
+// Delete user
+//
+export async function deleteUser(req: Request, res: Response) {
+    let user: User = getRequestingUser(req);
+
+    if (user.pseudo !== req.params.pseudo) {
+        return res.status(403).json({
+            message: "Action not allowed"
+        });
+    }
+
+    const conn = await DbConnection.getConnection();
+    const userRepository = conn.getRepository(User);
+    const groupsToUsersRepository = conn.getRepository(GroupsToUsers);
+    await User.deleteUser(userRepository, groupsToUsersRepository, user.id);
+
+    return res.status(200).json({
+        message: "User deleted"
+    });
+}
+
 
 //
 // Get all groups a User belongs to
