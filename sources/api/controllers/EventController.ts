504da2ef0dcbbe07f8176776a208e9eba74b0e1f
matthias alif <matthias.alif@epitech.eu>
2018-12-04 11:26:23 -0500

    Add event related routes
    #98

---
+++ sources/api/controllers/EventController.ts
@@ -0,0 +1,81 @@
+import { Request, Response } from "express"
+
+
+export async function getEventInfo(req: Request, res: Response) {
+    return res.status(200).json({
+        message: "OK",
+        event: {
+            id: 5,
+            name: "Nouvel An",
+            description: "10, 9, 8, 7, 6, 5, 4, 3, 2, 1, BONNE ANNEEEE",
+            calendar_id: 2,
+            start_time: Date.parse("31-12-2018T20:00:00"),
+            end_time: Date.parse("01-01-2019T06:00:00")
+        }
+    });
+}
+
+export async function getEventAttendees(req: Request, res: Response) {
+    return res.status(200).json({
+        message: "OK",
+        members: [
+            {
+                pseudo: "Michel"
+            },
+            {
+                pseudo: "Nicolas"
+            },
+            {
+                pseudo: "Damien"
+            },
+            {
+                pseudo: "Henri"
+            }
+        ]
+    });
+}
+
+// The user is automatically added to members, it will change with roles
+export async function inviteUser(req: Request, res: Response) {
+    return res.status(200).json({
+        message: "OK",
+        members: [
+            {
+                pseudo: "Michel"
+            },
+            {
+                pseudo: "Corentin"
+            },
+            {
+                pseudo: "Nicolas"
+            },
+            {
+                pseudo: "Damien"
+            },
+            {
+                pseudo: "Henri"
+            }
+        ]
+    });
+
+}
+
+export async function editEvent(req: Request, res: Response) {
+    return res.status(200).json({
+        message: "OK",
+        event: {
+            id: 5,
+            name: "Nouvel An",
+            description: "10, 9, 8, 7, 6, 5, 4, 3, 2, 1, BONNE ANNEEEE",
+            calendar_id: 2,
+            start_time: Date.parse("31-12-2018T21:00:00"),
+            end_time: Date.parse("01-01-2019T07:00:00")
+        }
+    });
+}
+
+export async function deleteEvent(req: Request, res: Response) {
+    return res.status(200).json({
+        message: "Event successfully deleted"
+    });
+}
---
+++ sources/api/routes/events/index.ts
@@ -0,0 +1,19 @@
+import { Router } from "express";
+import * as AsyncHandler from "express-async-handler";
+
+import { loggedOnly } from "../../middleware/loggedOnly";
+import * as EventController from "../../controllers/EventController";
+
+const router = Router();
+
+router.get('/:event_id', AsyncHandler(loggedOnly), AsyncHandler(EventController.getEventInfo));
+
+router.get('/:event_id/attendees', AsyncHandler(loggedOnly), AsyncHandler(EventController.getEventAttendees));
+
+router.post('/:event_id/invite-user', AsyncHandler(loggedOnly), AsyncHandler(EventController.inviteUser));
+
+router.put('/:event_id', AsyncHandler(loggedOnly), AsyncHandler(EventController.editEvent));
+
+router.delete('/:event_id', AsyncHandler(loggedOnly), AsyncHandler(EventController.deleteEvent));
+
+export default router;
