0d9f8623f713e7ad274aad28169d3c491049779a
matthias alif <matthias.alif@epitech.eu>
2018-12-06 10:41:01 -0500

    Add formatEventList
    #113

--- sources/api/views/CalendarView.ts
+++ sources/api/views/CalendarView.ts
@@ -1,4 +1,5 @@
 import { Calendar } from "../../model/entity/Calendar";
+import { Event } from "../../model/entity/Event";
 
 export class CalendarView {
 
@@ -6,6 +7,21 @@ export class CalendarView {
         return {
             id: calendar.id,
             name: calendar.name
+        };
+    }
+
+    public static formatEventRecap(event: Event): any {
+        return {
+            id: event.id,
+            name: event.name,
+            start_time: event.startTime,
+            end_time: event.endTime,
+        };
+    }
+
+    public static formatEventList(events: Event[]): any {
+        return {
+            events: events.map(this.formatEventRecap),
         };
     }
 }
