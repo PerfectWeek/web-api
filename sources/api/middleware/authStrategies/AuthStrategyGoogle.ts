c13bc4f444e732d940a3512cbbac28447199672d
Guillaume Benard <guillaume.benard@epitech.eu>
2019-05-04 23:56:18 -0400

    [OAuth] Google
    - Change route to receive access and
    refresh token
    - Do not override previous data

--- sources/api/middleware/authStrategies/AuthStrategyGoogle.ts
+++ sources/api/middleware/authStrategies/AuthStrategyGoogle.ts
@@ -5,6 +5,7 @@ import * as Jwt from "jsonwebtoken";
 
 import { User } from "../../../model/entity/User";
 import { UserView } from "../../views/UserView";
+import { ProviderPayload } from "../../../utils/types/ProviderPayload";
 
 
 type Params = {
@@ -29,59 +30,70 @@ export const init = (params: Params): void => {
     const scope = "https://www.googleapis.com/auth/calendar " +
         "https://www.googleapis.com/auth/userinfo.email " +
         "https://www.googleapis.com/auth/userinfo.profile";
-    const oauthUri = oauth.generateAuthUrl({
-        access_type: "offline",
-        scope: scope
-    });
-
-    params.express.app.get(params.express.path, (req: Request, res: Response) => {
-        res.status(200).json({
-            oauth_uri: oauthUri
-        });
-    });
 
     params.express.app.get(`${params.express.path}/callback`, (req: Request, res: Response) => {
-        oauth.getToken(req.query.code)
-            .then(token => {
-                oauth.setCredentials(token.tokens);
-                const googleOauthApi = google.oauth2({
-                    version: "v2",
-                    auth: oauth
-                });
 
-                googleOauthApi.userinfo.get().then(value => {
-                    const profile = value.data;
+        const accessToken = req.query.access_token;
+        const refreshToken = req.query.refreshToken || null;
 
-                    User.findByEmail(params.conn, profile.email).then((user: User) => {
-                        if (!user) {
-                            user = new User(profile.email, profile.email, null);
-                        }
+        if (!accessToken) {
+            return res.status(400).json({
+                message: "Bad request"
+            });
+        }
 
-                        user.googleProviderPayload = {
-                            accessToken: token.tokens.access_token,
-                            refreshToken: token.tokens.refresh_token,
-                            scope: scope,
-                            tokenType: token.tokens.token_type,
-                            expiresIn: null
-                        };
+        oauth.setCredentials({
+            access_token: accessToken,
+            refresh_token: refreshToken
+        });
+        const googleOauthApi = google.oauth2({
+            version: "v2",
+            auth: oauth
+        });
+        googleOauthApi.userinfo.get().then(value => {
+            const profile = value.data;
 
-                        params.conn.getRepository(User).save(user).then(() => {
-                            const token_payload = { id: user.id };
-                            const jwt = Jwt.sign(token_payload, process.env.JWT_ENCODE_KEY);
+            User.findByEmail(params.conn, profile.email).then((user: User) => {
+                if (!user) {
+                    user = new User(profile.email, profile.email, null);
+                }
 
-                            res.status(200).json({
-                                message: "Connected",
-                                token: jwt,
-                                user: UserView.formatUser(user)
-                            });
-                        });
-                    })
-                })
-            })
-            .catch(e => {
-                res.status(400).json({
-                    message: e.message
+                if (!user.googleProviderPayload) {
+                    user.googleProviderPayload = emptyPayloadToken(scope);
+                }
+
+                user.googleProviderPayload.accessToken = accessToken;
+                if (refreshToken) {
+                    user.googleProviderPayload.refreshToken = refreshToken;
+                }
+
+                params.conn.getRepository(User).save(user).then(() => {
+                    const token_payload = { id: user.id };
+                    const jwt = Jwt.sign(token_payload, process.env.JWT_ENCODE_KEY);
+
+                    res.status(200).json({
+                        message: "Connected",
+                        token: jwt,
+                        user: UserView.formatUser(user)
+                    });
                 });
             });
+        })
+        .catch(e => {
+            res.status(400).json({
+                message: e.message
+            });
+        });
     });
+};
+
+
+const emptyPayloadToken = (scope: string): ProviderPayload => {
+    return {
+        accessToken: null,
+        refreshToken: null,
+        scope: scope,
+        expiresIn: null,
+        tokenType: null
+    };
 };
